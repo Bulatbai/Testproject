008a9c89a4e06044dde174be2664bb49163f0898
Bulatbai <95519049+Bulatbai@users.noreply.github.com>
2022-09-09 15:50:04 +0600

    CReate Pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+
+ 
 from  . import views
+ 
 urlpatterns = [
      
     path('upload/', views.image_upload_view),
-    path('blogi/', views.Blogi, name='blog'),
+    path('blogi/', views.Blogi, name='search-element'),
     path('blogi/<int:id>/', views.detail_post, name='place'),
     path('blogi/<int:id>/edit/', views.edit_place, name='edit-place'),
     path('<int:id>/delete/', views.delete_place, name='del_place' ),
+    # path('search/', views.Search, name='search-element')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
+from msilib.schema import ListView
+from multiprocessing import context
+from pyexpat import model
+from turtle import title
+from urllib import request
+from winreg import QueryValue
 from django.shortcuts import render, redirect
-
-from django.shortcuts import render,HttpResponse 
+from django.core.paginator import Paginator
+from django.shortcuts import render,HttpResponse
 from django.http import Http404
 from .forms import ImageForm
 from . import models
+from django.views.generic import ListView
+
+
 
 def image_upload_view(request):
     """Process images uploaded by users"""
@@ -23,9 +32,9 @@ def image_upload_view(request):
 
 
 
-def Blogi(request):
-    user = models.Image.objects.all()
-    return render(request, 'proverka.html', {'post': user})
+# def Blogi(request):
+#     user = models.Image.objects.all()
+#     return render(request, 'proverka.html', {'post': user})
    
 
 def detail_post(request, id):
@@ -70,3 +79,54 @@ def delete_place(request, id):
     place_delete = models.Image.objects.get(id=id)
     place_delete.delete()
     return redirect(Blogi)
+
+
+
+
+
+ 
+
+
+
+
+def Search(request):
+    QueryValue = request.GET.get('q', '')
+    if QueryValue:
+        post = models.Image.objects.filter(title__icontains=QueryValue)
+    else:
+        post = models.Image.objects.all()
+    return render(request, 'basesmy.html', {'post': post})
+
+
+# class Search(ListView):
+#     template_name = 'proverka.html'
+#     context_object_name = 'product'
+#     paginate_by = 1
+ 
+#     def get_queryset(self):    
+#           return  models.Image.objects.filter(title__icontains=self.request.GET.get('q'))
+
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['q'] = self.request.GET.get('q')
+#         return context
+ 
+
+def Blogi(request):
+    QueryValue = request.GET.get('q')
+    if QueryValue:
+        post = models.Image.objects.filter(title__icontains=QueryValue)
+    else:
+        post = models.Image.objects.all()
+   
+
+    # contact_list = models.Image.objects.all()
+    paginator = Paginator(post, 3) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'proverka.html', {'post': page_obj})
+  
+
+
+
