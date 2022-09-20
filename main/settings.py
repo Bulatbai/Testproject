2d43a31226326c68228ba7d34b32d6a8eab392d0
Bulatbai <95519049+Bulatbai@users.noreply.github.com>
2022-09-20 12:00:37 +0600

    set_up settings

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lr$aw^56@vne&t5djh1)o^xpt_#u82&s1=3k%cr=d)eq@m!eol
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
